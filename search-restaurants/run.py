5fcd01464e76d55d4595a5b99fa09e880ab307b4
thoughtless02 <62426189+thoughtless02@users.noreply.github.com>
2020-09-13 16:01:59 +0530

    Modified run.py

--- search-restaurants/run.py
+++ search-restaurants/run.py
@@ -7,11 +7,8 @@ app = Flask(__name__)
 def home():
 	if request.method=='POST':
 		address = request.form['address']
-		Cuisines=request.form['Cuisines']
-		Sort_By=request.form['SortBy']
-		Sort_Order=request.form['SortOrder']
+
 		count=request.form['Count']
-		q=request.form['Query']
 		headers={'Accept': "application/json","user-key": "6f1a6514744476d55591d9dd25198c4c"}
 		headers2 = {'Authorization': 'prj_live_pk_feddf0d04ae764d8157faf0d35e0926e711d8c8d',}
 		params2 = (('query', address),)
@@ -19,24 +16,32 @@ def home():
 		data2 = response2.json()
 		Lat = data2['addresses'][0]['latitude']
 		Long = data2['addresses'][0]['longitude']
-		params={'lat':Lat,'lon':Long,'cuisines':Cuisines,'sort':Sort_By,'order':Sort_Order,'count':count,'q':q}
+		params={'lat':Lat,'lon':Long,'count':count}
 		r=requests.get('https://developers.zomato.com/api/v2.1/search',headers=headers,params=params)
 
 		data=r.json()
 		data=data['restaurants']
 
 		result=[]
+		name = []
+		address = []
+		rating = []
+		images = ['static/food1.jpg','static/food2.jpg','static/food3.jpg','static/food4.jpg','static/food5.jpg','static/food6.jpg',
+		'static/food7.jpg','static/food8.jpg','static/food9.jpg','static/food10.jpg','static/food11.jpg','static/food12.jpg','static/food13.jpg','static/food14.jpg','static/food15.jpg']
 		for i in data:
+			name.append((i['restaurant']['name']))
+			address.append((i['restaurant']['location']['address']))
+			rating.append((i['restaurant']['user_rating']['aggregate_rating']))
 			result.append((i['restaurant']['name'],i['restaurant']['location']['address'],i['restaurant']['average_cost_for_two'],i['restaurant']['user_rating']['aggregate_rating']))
 
-		return render_template('response.html',data=result,length=len(result))
+		return render_template('cards.html',address=address,name=name,rating=rating,count=int(count),image_url=images)
 
 
 
 
 
 
-	return render_template('home.html',title='Find restaurants')
+	return render_template('index.html',title='Find restaurants')
 
 
 
